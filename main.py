028f44ca200cd6b5097572bae6e86f1aa121065d
handaoui-mohamed <handaoui.mohamed@gmail.com>
2018-03-26 23:36:27 +0100

    added the second patch

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import matplotlib.path as mplPath
 import numpy as np
 
-BOUNDS_FILE = 'bounds.xy'
-XYZ_FILE = 'N35W002.xyz'
-OUTPUT_FILE = 'output.xyz'
+BOUNDS_FILE = 'bounds.txt'
+XYZ_PATCH1 = 'N35W002.xyz'
+XYZ_PATCH2 = 'N35W001.xyz'
+OUTPUT_FILE = 'output.csv'
 
 
 def readBounds(file_name):
@@ -16,20 +17,34 @@ def readBounds(file_name):
     return mplPath.Path(np.array(bounds))
 
 
-def readXYZ(polygon):
+def readXYZ(polygon, file_name, mode):
     index = 0
-    with open(XYZ_FILE, 'r') as input:
-        points = []
-        with open(OUTPUT_FILE, 'w') as output:
+    with open(file_name, 'r') as input:
+        with open(OUTPUT_FILE, mode) as output:
             for line in input:
                 coordinates = line.strip().split(' ')
                 coordinates = (float(coordinates[0]), float(coordinates[1]))
                 index = index + 1
                 print(index)
                 if(polygon.contains_point(coordinates)):
+                    print(','.join(map(str, line.strip().split(' '))),
+                          file=output, end='\n')
+
+
+def extractLinesEvery(lineNumber):
+    index = 0
+    with open(OUTPUT_FILE, 'r') as input:
+        points = []
+        with open(str(lineNumber)+'%_'+OUTPUT_FILE, 'w') as output:
+            for line in input:
+                index = index + 1
+                print(index)
+                if(index % lineNumber == 0):
                     print(line, file=output, end='')
 
 
 if __name__ == '__main__':
-    polygon = readBounds(BOUNDS_FILE)
-    readXYZ(polygon)
+    # polygon = readBounds(BOUNDS_FILE)
+    # readXYZ(polygon, file_name=XYZ_PATCH1, mode='w')
+    # readXYZ(polygon, file_name=XYZ_PATCH2, mode='a')
+    extractLinesEvery(10)
